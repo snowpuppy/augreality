0f874dc11cffe29fff7b23ee4affcce34743e1b1
Steven Ellis <steven.ellis.d@gmail.com>
2014-04-11 20:42:13 -0400

    updated rendering code to work with networking code

--- headset/src/gpu_src/render/gameobject.cpp
+++ headset/src/gpu_src/render/gameobject.cpp
@@ -1,24 +1,103 @@
-#include "gameobject.h"
+#include "GameObject.h"
+#include <irrlicht.h>
+#include <unistd.h>
 #include <string>
+#include <iostream>
+#include <fstream>
 
-GameObject::GameObject() {
-	x = y = z = pitch = yaw = roll = scale = 0.0;
-	visible = threeD = false;
-	id = 0;
+irr::scene::ISceneManager* GameObject::smgr = NULL;
+std::map<std::string, irr::scene::IMesh*> GameObject::Meshes;
+
+GameObject::GameObject() { }
+/** \brief Primary GameObject constructor.
+ * \param name The name of the mesh file to load.
+ * \param position The initial position to place the object.
+ * \param rotation The rotation to initially apply to the object, about (X,Y,Z) axes.
+ * \param visible Whether or not the object should be diplayed by the application.
+ */
+GameObject::GameObject(std::string name,
+                       irr::core::vector3df position,
+                       irr::core::vector3df rotation,
+                       irr::core::vector3df scale,
+                       bool visible
+                      ) {
+    irr::scene::IMesh* mesh = NULL;
+
+    //check if the mesh has already been loaded
+    if (Meshes.count(name) > 0) {
+        //if it has, use it
+        mesh = Meshes[name];
+    } else {
+        //load mesh from file
+        if(access((name + ".obj").c_str(), R_OK) != -1)
+			mesh = smgr->getMesh((name + ".obj").c_str());
+    }
+
+    //create scene node from the mesh
+    node = smgr->addMeshSceneNode(mesh);
+
+    //disable lighting on the node
+    if(node != NULL) {
+        node->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+    }
+
+    setPosition(position);
+    setRotation(rotation);
+    setScale(scale);
+    setVisible(visible);
 }
 
-GameObject::GameObject(float x, float y, float z, float pitch, float yaw, float roll, bool visible, bool threeD, uint8_t id, std::string filename, float scale) {
-        this->x = x;
-        this->y = y;
-        this->z = z;
-        this->pitch = pitch;
-        this->yaw = yaw;
-        this->roll = roll;
-        this->visible = visible;
-        this->threeD = threeD;
-        this->id = id;
-        this->filename = filename;
-        this->scale = scale;
-        this->mesh = new ngl::Obj(MODEL_DIR + filename + ".obj", TEXTURE_DIR + filename + ".jpg");
-        this->mesh->createVAO();
+void GameObject::setPosition(irr::core::vector3df position) {
+    node->setPosition(position);
+}
+
+void GameObject::setRotation(irr::core::vector3df rotation) {
+    node->setRotation(rotation);
+}
+
+void GameObject::setPosition(float x, float y , float z) {
+    node->setPosition(irr::core::vector3df(x, y, z));
+}
+
+void GameObject::setRotation(float x, float y , float z) {
+    node->setPosition(irr::core::vector3df(x, y, z));
+}
+
+void GameObject::setScale(irr::core::vector3df scale) {
+    node->setScale(scale);
+}
+
+void GameObject::setVisible(bool visible) {
+    node->setVisible(visible);
+}
+
+void GameObject::setSceneManager(irr::scene::ISceneManager* manager) {
+    smgr = manager;
+}
+
+/** \brief Fills an array of GameObjects with those described in a file.
+ * \param filename The filename of the configuration file to load.
+ * \param objects The array of GameObjects to be written to.
+ */
+void GameObject::loadConfigFile(std::string filename, GameObject *objects) {
+    int index;
+    bool threed;
+    float locx, locy, locz, orx, ory, orz;
+    bool show;
+    float scale;
+    std::string id;
+
+    std::ifstream file(filename.c_str());
+
+    //reads each game object into the objects array
+    while(file >> index) {
+        file >> threed >> locx >> locy >> locz >> orx >> ory >> orz >> filename >> show >> scale;
+        objects[index] = GameObject(filename,
+                                    irr::core::vector3df(locx, locy, locz),
+                                    irr::core::vector3df(orx, ory, orz),
+                                    irr::core::vector3df(scale, scale,scale),
+                                    show);
+    }
+    
+    
 }
--- headset/src/gpu_src/render/gameobject.h
+++ headset/src/gpu_src/render/gameobject.h
@@ -1,42 +1,33 @@
-#ifndef GAMEOBJECT_H__
-#define GAMEOBJECT_H__
-
+#include <irrlicht.h>
 #include <string>
-#include <ngl/Obj.h>
-#define MODEL_DIR "models/"
-#define TEXTURE_DIR "textures/"
+#include <map>
 
-typedef unsigned char uint8_t;
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
 
 class GameObject {
+private:
+    static std::map<std::string, irr::scene::IMesh*> Meshes;
+    static irr::scene::ISceneManager* smgr;
+
+    irr::scene::ISceneNode* node;
 
 public:
-        //constructors
-        GameObject();
-        GameObject(float x, float y, float z, float pitch, float yaw, float roll, bool visible, bool threeD, uint8_t id, std::string filename, float scale);
+    GameObject();
+    GameObject(std::string name,
+               irr::core::vector3df position = irr::core::vector3df(0.0f,0.0f,0.0f),
+               irr::core::vector3df rotation = irr::core::vector3df(0.0f,0.0f,0.0f),
+               irr::core::vector3df scale = irr::core::vector3df(1.0f,1.0f,1.0f),
+               bool visible = true);
+    void setPosition(irr::core::vector3df position);
+    void setRotation(irr::core::vector3df rotation);
+    void setPosition(float, float, float);
+    void setRotation(float, float, float);
+    void setScale(irr::core::vector3df scale);
+    void setVisible(bool visible);
 
-        void update(float x, float y, float pitch, float yaw, float roll);
-
-        //accessors
-        float getX() {return x;}
-        float getY() {return y;}
-        float getPitch() {return pitch;}
-        float getRoll() {return roll;}
-        float getYaw() {return yaw;}
-        bool isVisible() {return visible;}
-
-        float x;
-        float y;
-        float z;
-        float pitch;
-        float roll;
-        float yaw;
-        uint8_t id;
-        bool visible;
-        bool threeD;
-        float scale;
-        std::string filename;
-        ngl::Obj *mesh;
+    static void loadConfigFile(std::string filename, GameObject *objects);
+    static void setSceneManager(irr::scene::ISceneManager* manager);
 };
 
 #endif
