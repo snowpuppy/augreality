b438c679cf1b9ba20bd1ebffc9f740bdd26516cc
Thor Smith <smith777@purdue.edu>
2014-04-16 13:13:28 -0400

    One ghost successfully moving in maze.

--- headset/src/simulation_src/testApplication.py
+++ headset/src/simulation_src/testApplication.py
@@ -28,33 +28,43 @@ def initGhosts(objs):
 
 def moveGhosts(objs):
 	# move ghost by .2 meters (2 decimeters)
-	ghosts = objs['ghost']:
+	ghosts = objs['ghost']
+	global ghost1State
+	global ghost2State
+	global ghost3State
+	global ghost4State
 	# Ghost1
-		if (ghost1State == 0):
-			if (ghosts[0].x3 > -2):
-				ghosts[0].x3 -= .4;
-			else:
-				ghost1State = 1
-		if (ghost1State == 1):
-			if (ghosts[0].y3 > -20):
-				ghosts[1].y3 -= .4;
-			else:
-				ghost1State = 2
-		if (ghostState == 2):
-			if (ghosts[0].x3 < 22):
-				ghosts[0].x3 += .4
-			else
-				ghost1State = 3
-		if (ghostState == 3):
-			if (ghosts[0].y3 < -3):
-				ghosts[0].y3 += .4;
-			else:
-				ghostState = 4
-		if (ghostState == 4):
-			if (ghosts[0].x3 > -2):
-				ghosts[0].x3 -= .4;
-			else:
-				ghostState = 1
+	if (ghost1State == 0):
+		ghosts[0].yaw = 180
+		if (ghosts[0].x3 > -2):
+			ghosts[0].x3 -= .4;
+		else:
+			ghost1State = 1
+	if (ghost1State == 1):
+		ghosts[0].yaw = -90
+		if (ghosts[0].y3 > -21):
+			ghosts[0].y3 -= .4;
+		else:
+			ghost1State = 2
+	if (ghost1State == 2):
+		ghosts[0].yaw = 0
+		if (ghosts[0].x3 < 22):
+			ghosts[0].x3 += .4
+		else:
+			ghost1State = 3
+	if (ghost1State == 3):
+		ghosts[0].yaw = 90
+		if (ghosts[0].y3 < -7):
+			ghosts[0].y3 += .4;
+		else:
+			ghost1State = 4
+	if (ghost1State == 4):
+		ghosts[0].yaw = 180
+		if (ghosts[0].x3 > -2):
+			ghosts[0].x3 -= .4;
+		else:
+			ghost1State = 1
+	print "GhostState:", ghost1State, "x3:",x3,"y3:",y3
 	# Ghost2
 	# Ghost3
 	# Ghost4
@@ -89,10 +99,10 @@ pitch = 0
 yaw = 0
 # 74 and 75 are ghosts
 #	def __init__(self, instId, typeShow, x2, y2, x3, y3, z3, roll, pitch, yaw, scale, name):
-initGhosts(objs)
+initGhosts(myObjs)
 while (1):
-	moveGhosts();
-	sendUpdateObjsGpu(2, myObjs['ghost'])
+	moveGhosts(myObjs);
+	sendUpdateObjsGpu(4, myObjs['ghost'])
 	time.sleep(.2)
 time.sleep(3.0)
 gpuQuit()
